9b9ab892b326b7a71abb04c01b5fdaf1949cf72d
francosauvisky <francosauvisky@gmail.com>
2017-05-22 16:50:39 -0300

    float calculations & nicer code

--- main.c
+++ main.c
@@ -12,6 +12,43 @@ with this source code in the file LICENSE.md
 #define F_CPU 8000000UL
 #include <util/delay.h>
 
+#define BAUD 19200
+#include <util/setbaud.h>
+
+void uart_print(const char *);
+void uart_print_long_dec(uint16_t, uint8_t);
+void setup_RS232(void);
+void flush_UART_RX_buffer(void);
+float getADC(uint16_t);
+
+void
+main (void)
+{
+	setup_RS232();
+	flush_UART_RX_buffer();
+
+	// Setting up ADC:
+	ADMUX = _BV(REFS0) | _BV(REFS1); // ADC0 as input, VREF = 2.56V
+	ADCSRA |= _BV(ADPS0) | _BV(ADPS2); // Clock prescaler = 1/64
+	ADCSRA |= _BV(ADEN); // ADC Enable
+
+	float temp_coeff_zero = 544; // ADC value at 0°C
+	float temp_coeff_dx = -0.5; // Unit of °C per ADC unit
+
+	// Two-point calibration: measure ADC values V1, V2 on temperatures T1, T2 respectively
+	// temp_coeff_dx = (T1 - T2)/(V2 - V1);
+	// temp_coeff_zero = (V1*T2 - V2*T1)/(T2 - T1);
+
+	while(1)
+	{
+		float temp = (temp_coeff_zero - getADC(1000)) * temp_coeff_dx;
+
+		uart_print("Temp: ");
+		uart_print_long_dec((uint16_t) 10*temp, 3);
+		uart_print("°C\r\n");
+	}
+}
+
 void
 uart_print(const char *string) // Simple function for printing a string
 {
@@ -70,10 +107,8 @@ flush_UART_RX_buffer(void)
 	}
 }
 
-// Using multiplier != 1 is useful for oversampling
-// If no oversampling is requires, use multiplier = 1, equivalent to an arithmetic mean
-uint32_t
-getADC(uint16_t iter, uint16_t multiplier)
+float
+getADC(uint16_t iter)
 {	
 	uint32_t ADCVal = 0;
 	for(uint32_t i = 0; i < iter; i++)
@@ -83,44 +118,5 @@ getADC(uint16_t iter, uint16_t multiplier)
 		ADCVal += ADCW;
 	}
 
-	return (ADCVal/(iter/multiplier));
-}
-
-void
-main (void)
-{
-	setup_RS232();
-	flush_UART_RX_buffer();
-
-	// Setting up ADC:
-	ADMUX = _BV(REFS0) | _BV(REFS1); // ADC0 as input, VREF = 2.56V
-	ADCSRA |= _BV(ADPS0) | _BV(ADPS2); // Clock prescaler = 1/64
-	ADCSRA |= _BV(ADEN); // ADC Enable
-
-	uint16_t temp_coeff_zero = 544; // ADC value at 0°C
-	uint8_t temp_coeff_dx = 50; // Unit of -0.01°C per ADC unit
-	// Two-point calibration: measure ADC values V1, V2 on temperatures T1, T2 respectively
-	// temp_coeff_dx = (T1 - T2)/(V2 - V1)
-	// temp_coeff_zero = (V1*T2 - V2*T1)/(T2 - T1)
-
-	while(1)
-	{
-		// unit(temp_coeff_zero) = ADC_unit
-		// unit(getADC(..., multiplier)) = ADC_unit/multiplier
-		// So we need to multiply temp_coeff_zero by multiplier before taking the difference
-
-		// unit(temp_coeff_dx) = 0.01 K/ADC_unit
-		// unit( (multiplier*temp_coeff_zero - getADC(..., multiplier)) * temp_coeff_dx ) = 0.01K / multiplier
-		// And we need to divide temp by 10*multiplier to get 0.1 K
-
-		// Seems contradictory, but what happens is that physically we want to multiply by 1 (as we don't want
-		// to change the physical value), so, for example, when we multiply temp_coeff_zero by "multiplier",
-		// the units of that constant is 1/multiplier, equivalent to a physical 1 (multiplicative identity)
-
-		uint16_t temp = ( 10*temp_coeff_zero - getADC(1000, 10) ) * temp_coeff_dx/100; // unit: 0.1°C
-
-		uart_print("Temp: ");
-		uart_print_long_dec(temp, 3);
-		uart_print("°C\r\n");
-	}
+	return ((float) ADCVal)/iter;
 }
